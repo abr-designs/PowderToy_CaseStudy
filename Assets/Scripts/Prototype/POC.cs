cdad341851e1b2ada4405e5a0d37b4d293b8772c
Alex Bedard-Reid <alexbedardreid@gmail.com>
2022-09-10 16:36:50 -0700

    Added Plugins

---
+++ Assets/Scripts/Prototype/POC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PowderToy.Prototype
+{
+    public class POC : MonoBehaviour
+    {
+        private Vector2Int size;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
