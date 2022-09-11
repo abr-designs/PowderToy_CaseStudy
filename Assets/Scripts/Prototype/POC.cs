9569818dba2204ddd0c80a8046bf5a5a1dca6f08
Alex Bedard-Reid <alexbedardreid@gmail.com>
2022-09-10 17:21:37 -0700

    Simple Setup Texture2D

--- Assets/Scripts/Prototype/POC.cs
+++ Assets/Scripts/Prototype/POC.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,38 @@ namespace PowderToy.Prototype
 {
     public class POC : MonoBehaviour
     {
+        private static readonly int BaseMapPropertyID = Shader.PropertyToID("_BaseMap");
+        
+        [SerializeField, Min(0)]
         private Vector2Int size;
-        // Start is called before the first frame update
-        void Start()
-        {
+        [SerializeField]
+        private Renderer targetRenderer;
+        private Material _sharedMaterial;
 
+        [SerializeField]
+        private Color setColor = Color.black;
+        
+        [SerializeField]
+        private Vector2Int coordinate;
+        
+        private Texture2D _testTexture;
+        private Texture2D _emptyTexture;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _sharedMaterial = targetRenderer.sharedMaterial;
+            
+            _emptyTexture = _testTexture = new Texture2D(size.x, size.y, TextureFormat.RGBA4444, false);
+            
         }
 
-        // Update is called once per frame
-        void Update()
+        private void LateUpdate()
         {
-
+            _testTexture.SetPixels(_emptyTexture.GetPixels());
+            _testTexture.SetPixel(coordinate.x, coordinate.y, Color.white);
+            
+            _sharedMaterial.SetTexture(BaseMapPropertyID, _testTexture);
         }
     }
 }
